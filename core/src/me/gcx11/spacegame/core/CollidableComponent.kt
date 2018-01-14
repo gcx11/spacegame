95627f266da32d14c6a574afffda4aa4200b4093
hanpari <hanpari@users.noreply.github.com>
2018-01-14 21:07:57 +0100

    Update CollidableComponent.kt

--- core/src/me/gcx11/spacegame/core/CollidableComponent.kt
+++ core/src/me/gcx11/spacegame/core/CollidableComponent.kt
@@ -1,5 +1,5 @@
 package me.gcx11.spacegame.core
 
 interface CollidableComponent : Component {
-    fun collideWith(component: CollidableComponent): Boolean
+    fun collidesWith(component: CollidableComponent): Boolean
 }
