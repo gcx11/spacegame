cc4ab9f25786760b30d501bcf87c2526ed7eaa61
hanpari <hanpari@seznam.cz>
2018-01-22 10:43:07 +0100

    Unit tests for Complex shape

--- core/test/CollisionUtilsTest.kt
+++ core/test/CollisionUtilsTest.kt
@@ -1,13 +1,32 @@
 import com.badlogic.gdx.math.Vector2
-import me.gcx11.spacegame.core.Line
-import me.gcx11.spacegame.core.Point
-import me.gcx11.spacegame.core.Shape
-import me.gcx11.spacegame.core.Triangle
+import me.gcx11.spacegame.core.*
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class CollisionUtilsTest {
+
+    val vec00 = Vector2(0F, 0F)
+    val vec11 = Vector2(1F, 1F)
+    val vec22 = Vector2(2F, 2F)
+    val vec99 = Vector2(9F, 9F)
+    val vec09 = Vector2(0F, 9F)
+    val vec90 = Vector2(9F, 0F)
+    val vec_50_50 = Vector2(50F, 50F)
+
+    val point00 = Point(vec00)
+    val point11 = Point(vec11)
+    val point99 = Point(vec99)
+    val point09 = Point(vec09)
+    val point90 = Point(vec90)
+    val point_50_50 = Point(vec_50_50)
+
+    val line00to11 = Line(vec00, vec11)
+    val triangle00_09_90 = Triangle(vec00, vec09, vec90)
+    val triangle99_09_90 = Triangle(vec99, vec09, vec90)
+
+    // Rectangle of 00 to 99
+    val complexRectangle_00_99 = Complex(setOf(triangle00_09_90))
 
     private fun checkIntersectionBothWays(first: Shape, second: Shape): Boolean {
         return first.intersectsWith(second) && second.intersectsWith(first)
@@ -80,13 +99,20 @@ class CollisionUtilsTest {
     fun triangleWithTriangle() {
         val first = Triangle(Vector2(0f, 0f), Vector2(1f, 0f), Vector2(0f, 1f))
         val second = Triangle(
-            Vector2(0.25f, 0.25f), Vector2(1.25f, 0.25f), Vector2(0.25f, 1.25f)
+                Vector2(0.25f, 0.25f), Vector2(1.25f, 0.25f), Vector2(0.25f, 1.25f)
         )
         val third = Triangle(
-            Vector2(-0.25f, -0.25f), Vector2(-1.25f, -0.25f), Vector2(-0.25f, -1.25f)
+                Vector2(-0.25f, -0.25f), Vector2(-1.25f, -0.25f), Vector2(-0.25f, -1.25f)
         )
 
         assertTrue(checkIntersectionBothWays(first, second))
         assertFalse(checkIntersectionBothWays(first, third))
+    }
+
+    @Test
+    fun complexShapes() {
+        assertTrue(checkIntersectionBothWays(complexRectangle_00_99, point11))
+        assertTrue(checkIntersectionBothWays(complexRectangle_00_99, complexRectangle_00_99))
+        assertFalse(checkIntersectionBothWays(complexRectangle_00_99, point_50_50))
     }
 }
