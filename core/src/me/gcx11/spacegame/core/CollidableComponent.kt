9031688e3bb8cf9131dd022bb23cd9e98fd8cd24
gcx11 <gcx11@seznam.cz>
2018-01-23 00:09:40 +0100

    fixed too specific parameter

--- core/src/me/gcx11/spacegame/core/CollidableComponent.kt
+++ core/src/me/gcx11/spacegame/core/CollidableComponent.kt
@@ -3,6 +3,12 @@ package me.gcx11.spacegame.core
 interface CollidableComponent : DisposableComponent {
     val collidedCollection: MutableCollection<CollidableComponent>
 
+    fun collidesWith(collidable: CollidableComponent): Boolean {
+        val selfGeo = parent.getRequiredComponent<GeometricComponent>()
+        val otherGeo = collidable.parent.getOptionalComponent<GeometricComponent>()
+        return otherGeo?.shape?.intersectsWith(selfGeo.shape) ?: false
+    }
+
     fun addCollided(collidable: CollidableComponent) = collidedCollection.add(collidable)
 
     fun clearAllCollided() = collidedCollection.clear()
@@ -10,12 +16,4 @@ interface CollidableComponent : DisposableComponent {
     fun allCollided(): Collection<CollidableComponent> = collidedCollection
 
     override fun dispose() = clearAllCollided()
-
-    fun collidesWith(collidable: CollidableComponent): Boolean {
-        val selfGeo = parent.getRequiredComponent<GeometricComponent>()
-        val otherGeo = collidable
-            .parent
-            .getOptionalComponent<me.gcx11.spacegame.spaceship.GeometricComponent>()
-        return otherGeo?.shape?.intersectsWith(selfGeo.shape) ?: false
-    }
 }
