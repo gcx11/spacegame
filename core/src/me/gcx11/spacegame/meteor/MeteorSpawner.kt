a98cf02bde112232972f9b41506e85f911e6f3ba
gcx11 <gcx11@seznam.cz>
2018-01-28 21:55:08 +0100

    added meteor

---
+++ core/src/me/gcx11/spacegame/meteor/MeteorSpawner.kt
@@ -0,0 +1,15 @@
+package me.gcx11.spacegame.meteor
+
+import me.gcx11.spacegame.core.Entity
+
+object MeteorSpawner {
+
+    fun createMeteor(x: Float, y: Float): Entity {
+        return Entity.new().also {
+            it.addComponent(GeometricComponent(it, x, y, radius = 40f))
+            it.addComponent(CollidableComponent(it))
+            it.addComponent(RenderableComponent(it))
+            it.addComponent(MoveBehaviourComponent(it))
+        }
+    }
+}
