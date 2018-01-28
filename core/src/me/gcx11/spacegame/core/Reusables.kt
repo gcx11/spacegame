7d0abada7f716cdeaee539c95b4c8b4947dcb2db
hanpari <hanpari@seznam.cz>
2018-01-28 17:35:18 +0100

    Documented Reusable - sample for future

--- core/src/me/gcx11/spacegame/core/Reusables.kt
+++ core/src/me/gcx11/spacegame/core/Reusables.kt
@@ -3,6 +3,18 @@ package me.gcx11.spacegame.core
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
+/**
+ * Reusable - a delegated property
+ *
+ * This class is intented to be used as delegated property.
+ * Its main purpose is to avoid creating unnecessary instances;
+ * and thus prevent lagging and excessive memory usage.
+ *
+ * @param T the type of a class owning delegated property.
+ * @param func block function updating a instance
+ * @param default the default value for a instance
+ * @sample [me.gcx11.spacegame.spaceship.GeometricComponent.center]
+ */
 class Reusable<T>(
     val default: T,
     val func: T.() -> Unit
