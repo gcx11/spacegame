cf82f5d97f6583a4e4270bef50ae317134db1026
gcx11 <gcx11@seznam.cz>
2018-01-30 22:18:41 +0100

    changed collidable for meteor

--- core/src/me/gcx11/spacegame/meteor/MeteorSpawner.kt
+++ core/src/me/gcx11/spacegame/meteor/MeteorSpawner.kt
@@ -1,5 +1,6 @@
 package me.gcx11.spacegame.meteor
 
+import me.gcx11.spacegame.core.DefaultCollidableComponent
 import me.gcx11.spacegame.core.Entity
 
 object MeteorSpawner {
@@ -7,7 +8,7 @@ object MeteorSpawner {
     fun createMeteor(x: Float, y: Float): Entity {
         return Entity.new().also {
             it.addComponent(GeometricComponent(it, x, y, radius = 40f))
-            it.addComponent(CollidableComponent(it))
+            it.addComponent(DefaultCollidableComponent(it))
             it.addComponent(RenderableComponent(it))
             it.addComponent(MoveBehaviourComponent(it))
         }
