94b391691022197c6c538626ed1a2d4641081f3c
gcx11 <gcx11@seznam.cz>
2018-01-31 21:51:43 +0100

    WIP advanced enemy AI

--- core/src/me/gcx11/spacegame/spaceship/EnemyLogicComponent.kt
+++ core/src/me/gcx11/spacegame/spaceship/EnemyLogicComponent.kt
@@ -1,51 +1,37 @@
 package me.gcx11.spacegame.spaceship
 
-import me.gcx11.spacegame.SpaceGame
 import me.gcx11.spacegame.core.Entity
-import kotlin.math.abs
-import kotlin.math.atan2
 
 class EnemyLogicComponent(
     parent: Entity
 ) : LogicComponent(parent) {
-    private var target: Entity? = null
+    var currentState: EnemyBehaviourState = EnemyBehaviourState.WAITING
 
     override fun update(delta: Float) {
-        target = SpaceGame.entitiesReadOnly.firstOrNull { it.hasComponent<PlayerLogicComponent>() }
+        val newState = states.first { it.needActivate(this) }
+
+        if (newState != currentState) currentState.onDisable()
+        currentState = newState
+
+        currentState.updateLogic(this)
     }
 
     override fun computeDirection(): Float {
-        val selfGeo = parent.getRequiredComponent<GeometricComponent>()
-        val targetGeo = target?.getRequiredComponent<GeometricComponent>()
-
-        if (targetGeo != null) {
-            return atan2(targetGeo.y - selfGeo.y, targetGeo.x - selfGeo.x)
-        } else {
-            return selfGeo.directionAngle
-        }
-    }
-
-    override fun canFire(): Boolean {
-        return isBehindEnemy()
+        return currentState.computeDirection(this)
     }
 
     override fun canSpeedUp(): Boolean {
-        return isBehindEnemy()
+        return currentState.canSpeedUp(this)
     }
 
     override fun computeSpeedPercentage(): Float {
-        return if (isBehindEnemy()) 0.5f else 1.0f
+        return currentState.computeSpeedPercentage(this)
     }
 
-    private fun isBehindEnemy(): Boolean {
-        val selfGeo = parent.getRequiredComponent<GeometricComponent>()
-        val targetGeo = target?.getRequiredComponent<GeometricComponent>()
-
-        if (targetGeo != null) {
-            val enemyDirection = atan2(targetGeo.y - selfGeo.y, targetGeo.x - selfGeo.x)
-            return abs(selfGeo.directionAngle - enemyDirection) < 0.5f
-        } else {
-            return false
-        }
+    override fun canFire(): Boolean {
+        return currentState.canFire(this)
     }
+
+    var states = arrayOf(EnemyBehaviourState.WAITING, EnemyBehaviourState.ATTACKING)
+        .sortedByDescending { it.priority }
 }
